76eb660d78adb93dc61222ed0264411029d597d1
Mari Caramello <silvamariabrito20@gmail.com>
2017-08-23 11:44:42 -0300

    Update primeiroscomandos.pl

--- passo1/primeiroscomandos.pl
+++ passo1/primeiroscomandos.pl
@@ -5,9 +5,9 @@ uma_cidade(portonacional).
 
 
 % 1. ?- uma_cidade(X). : retorna a primeira cidade da lista;
-% 1.1 X = palmas; : mostra a cidade em sequência;
-% 2. ?- uma_cidade(palmas). : verifica se Palmas está na lista
+% 1.1 X = palmas; : mostra a cidade em sequÃªncia;
+% 2. ?- uma_cidade(palmas). : verifica se Palmas estÃ¡ na lista
 % retornando true ou false;
-% 3. ?- uma_cidade(X), write(X), nl, fail. : retorna toda a kista de
+% 3. ?- uma_cidade(X), write(X), nl, fail. : retorna toda a lista de
 % cidade.
 
